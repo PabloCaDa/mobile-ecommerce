17ebb58559078b36a857fca632d6f76c99d3c413
PabloCaDa <pablo.cabreradarias@gmail.com>
2025-02-27 14:21:23 +0000

    improve test imports

--- src/hooks/phones/usePhone.test.tsx
+++ src/hooks/phones/usePhone.test.tsx
@@ -1,7 +1,7 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import { useQuery } from "@tanstack/react-query";
 import { usePhone } from "./usePhone";
-import { IPhoneDetails } from "../../types/phone";
+import { IPhoneDetails } from "@/types/phone";
 import { mockPhoneFixture } from "@/test/__fixtures__/phones";
 
 jest.mock("@tanstack/react-query", () => ({
