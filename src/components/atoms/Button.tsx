f5fec4c99b384bb07389e21a8a1bae31926d9237
PabloCaDa <pablo.cabreradarias@gmail.com>
2025-02-27 14:33:19 +0000

    add button component

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,63 @@
+interface ButtonProps {
+  variant: "default" | "primary" | "inverse" | "danger";
+  size: "content" | "small" | "large";
+  children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  variant,
+  size,
+  children,
+  onClick,
+  className = "",
+  disabled = false,
+}) => {
+  let buttonStyle = "w-full border";
+
+  switch (variant) {
+    case "primary":
+      buttonStyle += ` py-[5px] px-[7px] border-black-100 bg-black-100 text-white  ${
+        disabled
+          ? "bg-grey-200 border-grey-200 text-grey-300"
+          : "bg-black-100 text-white"
+      }`;
+      break;
+    case "inverse":
+      buttonStyle += ` py-[5px] px-[7px] bg-white text-black-100 border-black-300 ${
+        disabled && "bg-grey-200 text-grey-300"
+      }`;
+      break;
+    case "danger":
+      buttonStyle += " border-0 py-[0px] px-[0px] text-red-500 text-start";
+      break;
+    default:
+      buttonStyle += " bg-gray-100 text-gray-700 hover:bg-gray-200";
+  }
+
+  switch (size) {
+    case "content":
+      buttonStyle += " text-xs";
+      break;
+    case "small":
+      buttonStyle += " h-[48px] text-xs";
+      break;
+    case "large":
+      buttonStyle += " h-[56px]";
+      break;
+    default:
+      buttonStyle += "";
+  }
+
+  return (
+    <button
+      className={`${buttonStyle} ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
