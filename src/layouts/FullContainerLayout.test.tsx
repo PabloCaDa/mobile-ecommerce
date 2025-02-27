cd21a21e3d04e72f0a5bcb59aa7580328734b504
PabloCaDa <pablo.cabreradarias@gmail.com>
2025-02-27 10:24:11 +0000

    improve tests

--- src/layouts/FullContainerLayout.test.tsx
+++ src/layouts/FullContainerLayout.test.tsx
@@ -3,22 +3,13 @@ import { FullContainerLayout } from "./FullContainerLayout";
 
 describe("FullContainerLayout", () => {
   it("renders children correctly", () => {
-    const { getByText } = render(
+    const { getByText, container } = render(
       <FullContainerLayout>
         <div>Test Child</div>
       </FullContainerLayout>
     );
 
     expect(getByText("Test Child")).toBeInTheDocument();
-  });
-
-  it("applies correct classes to the container", () => {
-    const { container } = render(
-      <FullContainerLayout>
-        <div>Test Child</div>
-      </FullContainerLayout>
-    );
-
     expect(container.firstChild).toHaveClass(
       "w-full px-1 lg:px-4 pb-1 mx-auto"
     );
--- src/layouts/RestrictedContainerLayout.test.tsx
+++ src/layouts/RestrictedContainerLayout.test.tsx
@@ -3,22 +3,13 @@ import { RestrictedContainerLayout } from "./RestrictedContainerLayout";
 
 describe("RestrictedContainerLayout", () => {
   it("renders children correctly", () => {
-    const { getByText } = render(
+    const { getByText, container } = render(
       <RestrictedContainerLayout>
         <div>Test Child</div>
       </RestrictedContainerLayout>
     );
 
     expect(getByText("Test Child")).toBeInTheDocument();
-  });
-
-  it("applies correct classes to the container", () => {
-    const { container } = render(
-      <RestrictedContainerLayout>
-        <div>Test Child</div>
-      </RestrictedContainerLayout>
-    );
-
     expect(container.firstChild).toHaveClass("lg:w-[1200px] mx-auto");
   });
 });
