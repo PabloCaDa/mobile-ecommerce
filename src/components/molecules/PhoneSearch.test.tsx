8b56d7211059f35ab769d65a94a772ec717ed0b0
PabloCaDa <pablo.cabreradarias@gmail.com>
2025-02-28 17:34:54 +0000

    add PhoneSearch component

---
+++ src/components/molecules/PhoneSearch.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { PhoneSearch } from "@/components/molecules/PhoneSearch";
+
+jest.mock("@/components/atoms/ResultsAmount", () => ({
+  ResultsAmount: jest.fn(() => <div>Mocked ResultsAmount</div>),
+}));
+
+describe("PhoneSearch", () => {
+  beforeEach(() => {
+    render(<PhoneSearch />);
+  });
+
+  it("renders correctly", () => {
+    expect(screen.getByPlaceholderText("Search phones")).toBeInTheDocument();
+    expect(screen.getByText("Mocked ResultsAmount")).toBeInTheDocument();
+  });
+
+  it("handles input change", () => {
+    const input = screen.getByPlaceholderText("Search phones...");
+
+    fireEvent.change(input, { target: { value: "test" } });
+    expect(input).toHaveValue("test");
+  });
+
+  it("handles clear search", () => {
+    const input = screen.getByPlaceholderText("Search phones");
+
+    fireEvent.change(input, { target: { value: "test" } });
+    fireEvent.change(input, { target: { value: "" } });
+    expect(input).toHaveValue("");
+  });
+});
---
+++ src/components/molecules/PhoneSearch.tsx
@@ -0,0 +1,29 @@
+import { Input } from "@/components/atoms/Input";
+import { ResultsAmount } from "../atoms/ResultsAmount";
+
+export const PhoneSearch = () => {
+  const handleInputChange = (inputValue: string) => {
+    console.log(inputValue);
+  };
+
+  const handleClearSearch = () => {
+    console.log("");
+  };
+
+  const resultsAmount = 10;
+
+  return (
+    <div className="w-full flex flex-col items-start sticky pt-1 z-10 bg-white">
+      <Input
+        onChange={handleInputChange}
+        onClear={handleClearSearch}
+        placeholder="Search phones..."
+        name="search"
+        ariaLabel="Search phones"
+        type={""}
+      />
+
+      {!!resultsAmount && <ResultsAmount resultsAmount={resultsAmount} />}
+    </div>
+  );
+};
