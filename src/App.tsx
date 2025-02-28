19181fc922688f8789609884b6320e326fa93334
PabloCaDa <pablo.cabreradarias@gmail.com>
2025-02-28 20:17:28 +0000

    add search context to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import routes from "@/routes";
 import { LayoutSwitcher } from "@/layouts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SearchProvider } from "@/contexts";
 
 const queryClient = new QueryClient();
 
@@ -9,13 +10,15 @@ function App() {
   return (
     <Router>
       <QueryClientProvider client={queryClient}>
-        <LayoutSwitcher>
-          <Routes>
-            {routes.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element} />
-            ))}
-          </Routes>
-        </LayoutSwitcher>
+        <SearchProvider>
+          <LayoutSwitcher>
+            <Routes>
+              {routes.map((route, index) => (
+                <Route key={index} path={route.path} element={route.element} />
+              ))}
+            </Routes>
+          </LayoutSwitcher>
+        </SearchProvider>
       </QueryClientProvider>
     </Router>
   );
