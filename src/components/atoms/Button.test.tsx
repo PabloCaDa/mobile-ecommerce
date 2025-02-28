f28239cfa04be681dd0fe0c4884dae8c49354f2a
Pablo Cabrera Darias <33932962+PabloCaDa@users.noreply.github.com>
2025-02-28 10:01:22 +0000

    Add Button component (#10)
    * add button component
    
    * improve accessibility
    
    * add disabled to danger variant

---
+++ src/components/atoms/Button.test.tsx
@@ -0,0 +1,122 @@
+import { render, fireEvent } from "@testing-library/react";
+import { Button } from "./Button";
+
+describe("Button component", () => {
+  it("renders correctly with default props", () => {
+    const { getByText } = render(
+      <Button
+        ariaLabel="button label"
+        onClick={() => {}}
+        size="content"
+        variant="default"
+      >
+        Default
+      </Button>
+    );
+
+    const button = getByText("Default");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass(
+      "w-full border bg-gray-100 text-gray-700 hover:bg-gray-200 text-xs"
+    );
+  });
+
+  it("renders correctly with primary variant", () => {
+    const { getByText } = render(
+      <Button
+        ariaLabel="button label"
+        onClick={() => {}}
+        size="small"
+        variant="primary"
+      >
+        Primary
+      </Button>
+    );
+
+    const button = getByText("Primary");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass(
+      "w-full border py-[5px] px-[7px] border-black-100 bg-black-100 text-white h-[48px] text-xs"
+    );
+  });
+
+  it("renders correctly with inverse variant", () => {
+    const { getByText } = render(
+      <Button
+        ariaLabel="button label"
+        onClick={() => {}}
+        size="large"
+        variant="inverse"
+      >
+        Inverse
+      </Button>
+    );
+
+    const button = getByText("Inverse");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass(
+      "w-full border py-[5px] px-[7px] bg-white text-black-100 border-black-300 h-[56px]"
+    );
+  });
+
+  it("renders correctly with danger variant", () => {
+    const { getByText } = render(
+      <Button
+        ariaLabel="button label"
+        onClick={() => {}}
+        size="content"
+        variant="danger"
+      >
+        Danger
+      </Button>
+    );
+
+    const button = getByText("Danger");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass(
+      "w-full border border-0 py-[0px] px-[0px] text-red-500 text-start text-xs"
+    );
+  });
+
+  it("handles click events", () => {
+    const handleClick = jest.fn();
+    const { getByText } = render(
+      <Button
+        ariaLabel="button label"
+        onClick={handleClick}
+        size="content"
+        variant="default"
+      >
+        Click me
+      </Button>
+    );
+
+    const button = getByText("Click me");
+    fireEvent.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it("applies disabled styles and does not handle click events when disabled", () => {
+    const handleClick = jest.fn();
+    const { getByText } = render(
+      <Button
+        ariaLabel="button label"
+        disabled
+        onClick={handleClick}
+        size="small"
+        variant="primary"
+      >
+        Disabled
+      </Button>
+    );
+
+    const button = getByText("Disabled");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass(
+      "w-full border py-[5px] px-[7px] border-black-100 bg-grey-200 border-grey-200 text-grey-300 h-[48px] text-xs"
+    );
+
+    fireEvent.click(button);
+    expect(handleClick).not.toHaveBeenCalled();
+  });
+});
---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,70 @@
+interface ButtonProps {
+  ariaLabel: string;
+  children: React.ReactNode;
+  onClick: () => void;
+  size: "content" | "small" | "large";
+  variant: "default" | "primary" | "inverse" | "danger";
+  className?: string;
+  disabled?: boolean;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  ariaLabel,
+  children,
+  className = "",
+  disabled = false,
+  onClick,
+  size,
+  variant,
+}) => {
+  let buttonStyle = "w-full border";
+
+  switch (variant) {
+    case "primary":
+      buttonStyle += ` py-[5px] px-[7px] border-black-100 bg-black-100 text-white  ${
+        disabled
+          ? "bg-grey-200 border-grey-200 text-grey-300"
+          : "bg-black-100 text-white"
+      }`;
+      break;
+    case "inverse":
+      buttonStyle += ` py-[5px] px-[7px] bg-white text-black-100 border-black-300 ${
+        disabled && "bg-grey-200 text-grey-300"
+      }`;
+      break;
+    case "danger":
+      buttonStyle += ` border-0 py-[0px] px-[0px]  text-start ${
+        disabled ? "text-grey-300" : "text-red-500"
+      }`;
+      break;
+    default:
+      buttonStyle += " bg-gray-100 text-gray-700 hover:bg-gray-200";
+  }
+
+  switch (size) {
+    case "content":
+      buttonStyle += " text-xs";
+      break;
+    case "small":
+      buttonStyle += " h-[48px] text-xs";
+      break;
+    case "large":
+      buttonStyle += " h-[56px]";
+      break;
+    default:
+      buttonStyle += "";
+  }
+
+  return (
+    <button
+      aria-disabled={disabled}
+      aria-label={ariaLabel}
+      className={`${buttonStyle} ${className}`}
+      onClick={!disabled ? onClick : undefined}
+      role="button"
+      tabIndex={disabled ? -1 : 0}
+    >
+      {children}
+    </button>
+  );
+};
