c2c8f6f63628d17a5e85fb2bcde9fc71c9625178
Pablo Cabrera Darias <33932962+PabloCaDa@users.noreply.github.com>
2025-03-01 19:23:31 +0000

    Add PhoneCard component (#21)
    * add PhoneCard component
    
    * move component to folder and add it to index

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,1 +1,2 @@
+export * from "./PhoneCard/PhoneCard";
 export * from "./PhoneSearch/PhoneSearch";
--- src/pages/Phones.tsx
+++ src/pages/Phones.tsx
@@ -1,3 +1,15 @@
+import { PhoneCard } from "@/components/molecules/PhoneCard/PhoneCard";
+import { SearchContext } from "@/contexts";
+import { usePhones } from "@/hooks/phones";
+import { use } from "react";
+
 export const Phones: React.FC = () => {
-  return <div>Phones</div>;
+  const { searchValue } = use(SearchContext);
+  const { phones } = usePhones(searchValue, 20);
+
+  return (
+    <div className="w-[250px] h-[250px] mt-3">
+      {phones.length > 0 && <PhoneCard phone={phones[0]} />}
+    </div>
+  );
 };
