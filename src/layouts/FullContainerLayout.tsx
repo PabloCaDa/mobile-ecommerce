cc60efc02e5d68faf61f1e23a57ab552c26ce2a1
PabloCaDa <pablo.cabreradarias@gmail.com>
2025-03-02 13:06:59 +0000

    adjust styles

--- src/layouts/FullContainerLayout.tsx
+++ src/layouts/FullContainerLayout.tsx
@@ -1,5 +1,7 @@
 import { PageLayoutProps } from "@/types";
 
 export const FullContainerLayout = ({ children }: PageLayoutProps) => {
-  return <div className="w-full px-1 lg:px-4 pb-1 mx-auto">{children}</div>;
+  return (
+    <div className="w-full px-1 lg:px-4 pb-1 lg:pb-2 mx-auto">{children}</div>
+  );
 };
