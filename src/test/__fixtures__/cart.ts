1ffda021f2e503a18cbf4e85013d9ca6a8c23299
PabloCaDa <pablo.cabreradarias@gmail.com>
2025-03-03 15:42:27 +0000

    add cartItem component

---
+++ src/test/__fixtures__/cart.ts
@@ -0,0 +1,24 @@
+import { ICartItem } from "@/types";
+
+export const cartFixture: ICartItem[] = [
+  {
+    ref: "1PhonecolorPhonestorage",
+    name: "Phone name",
+    color: "Phone color",
+    imageUrl: "Phone image",
+    price: 100,
+    storage: "Phone storage",
+    quantity: 1,
+    id: "1",
+  },
+  {
+    ref: "2Phone2colorPhone2storage",
+    name: "Phone 2 name",
+    color: "Phone 2 color",
+    imageUrl: "Phone 2 image",
+    price: 200,
+    storage: "Phone 2 storage",
+    quantity: 1,
+    id: "2",
+  },
+];
