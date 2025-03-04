055315b6ff4b59b96e93342b064fb6a29c47f4b2
PabloCaDa <pablo.cabreradarias@gmail.com>
2025-03-04 13:32:20 +0000

    add skeleton to cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -2,7 +2,36 @@ import { CartHeader } from "@/components/atoms";
 import { CartActions } from "@/components/molecules";
 import { CartItemList } from "@/components/organisms";
 import { CartContext } from "@/contexts";
-import { use } from "react";
+import { Suspense, use } from "react";
+import Skeleton from "react-loading-skeleton";
+
+export const CartSkeleton: React.FC = () => {
+  return (
+    <section role="region" className="flex flex-col min-h-[80vh]">
+      <h2 className="text-2xl" aria-label="Cart title">
+        <Skeleton width={200} />
+      </h2>
+      <CartItemListSkeleton />
+      <CartActionsSkeleton />
+    </section>
+  );
+};
+
+const CartItemListSkeleton: React.FC = () => (
+  <div className="flex-grow">
+    {[...Array(3)].map((_, index) => (
+      <div key={index} className="mb-4">
+        <Skeleton height={80} />
+      </div>
+    ))}
+  </div>
+);
+
+const CartActionsSkeleton: React.FC = () => (
+  <div className="mt-4">
+    <Skeleton height={50} />
+  </div>
+);
 
 export const Cart: React.FC = () => {
   const { cart } = use(CartContext);
@@ -13,10 +42,12 @@ export const Cart: React.FC = () => {
   );
 
   return (
-    <section role="region" className="flex flex-col min-h-[80vh]">
-      <CartHeader ammountOfItems={cart.length} />
-      <CartItemList cart={cart} />
-      <CartActions cartLength={cart.length} totalPrice={totalPrice} />
-    </section>
+    <Suspense fallback={<CartSkeleton />}>
+      <section role="region" className="flex flex-col min-h-[80vh]">
+        <CartHeader ammountOfItems={cart.length} />
+        <CartItemList cart={cart} />
+        <CartActions cartLength={cart.length} totalPrice={totalPrice} />
+      </section>
+    </Suspense>
   );
 };
